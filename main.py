0e840cf84576db2fda8debbeef2d696227316a64
Joanne Lee <vernapii@gmail.com>
2021-02-11 23:58:01 -0800

    frontend started

---
+++ main.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+
+class MyGridLayout(GridLayout):
+  def __init__(self, **kwargs):
+    # call grid layout in the constructor
+    super(MyGridLayout, self).__init__(**kwargs)
+
+    self.cols = 1
+    # input box
+    self.key = TextInput(multiline=False)
+    self.header = Label(text="Let's generate any scale!")
+    self.add_widget(self.header)
+    self.add_widget(self.key)
+
+    # submit button and bind button
+    self.submit = Button(text="Generate Key!")
+    self.submit.bind(on_press=self.press)
+    self.add_widget(self.submit)
+  
+  def press(self,instance):
+    key = self.key.text
+
+class ScaleGenerator(App):
+  def build(self):
+    return MyGridLayout()
+
+if __name__ == '__main__':
+  ScaleGenerator().run()
