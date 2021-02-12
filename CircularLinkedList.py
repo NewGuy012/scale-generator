6c999368a40dd75d2e75abd97fdd9930d97a2579
NewGuy012 <juyoung.m.yoo@gmail.com>
2021-02-12 02:50:50 -0800

    Modify function to take arguments. Added more scales.

--- CircularLinkedList.py
+++ CircularLinkedList.py
@@ -1,22 +1,27 @@
 class CircularLinkedList:
-    def __init__(self, nodes=None):
+    def __init__(self, node_str=None):
         self.head = None
+        self.node_dict = {}
 
-        if nodes is not None:
-            node = Node(data=nodes.pop(0))
+        if node_str is not None:
+            node = Node(data=node_str[0])
             self.head = node
-            for elem in nodes:
+            self.node_dict[node_str[0]] = node
+
+            for elem in node_str[1:]:
                 node.next = Node(data=elem)
+                node.next.previous = node
                 node = node.next
+                self.node_dict[elem] = node
+
             node.next = self.head
 
-    def __repr__(self, starting_point=None):
-        if starting_point is None:
-            starting_point = self.head
-        node = starting_point
-        nodes = [starting_point]
+    def __repr__(self):
+        starting_node = self.head
+        node = starting_node
+        nodes = [node]
 
-        while node is not None and (node.next != starting_point):
+        while node is not None and (node.next != starting_node):
             node = node.next
             nodes.append(node)
 
@@ -26,32 +31,50 @@ class CircularLinkedList:
         nodes = map(str, nodes)
         return " -> ".join(nodes)
 
-    def major_scale(self, starting_point=None):
-        if starting_point is None:
-            starting_point = self.head
-        node = starting_point
-        nodes = [starting_point]
+    def generate_scale(self, starting_node=None, scale_type=None):
+        if starting_node is None:
+            starting_node = self.head
+        else:
+            starting_node = self.node_dict[starting_node]
 
-        major_interval = ["W", "W", "H", "W", "W", "W", "H"]
+        node = starting_node
+        nodes = [starting_node]
 
-        for interval in major_interval:
-            if interval == "W":
-                node = node.next.next
-                nodes.append(node)
-            elif interval == "H":
+        scale_interval = {
+            "major": ["W", "W", "H", "W", "W", "W", "H"],
+            "minor": ["W", "H", "W", "W", "H", "W", "W"],
+            "pentatonic": ["W", "W", "WH", "W", "WH"],
+            "fourths": ["WWH"] * 12,
+            "fifths": ["WWWH"] * 12,
+            "relative minor": ["WWWWH"]
+        }
+        scale_interval = scale_interval.get(scale_type)
+
+        node_interval = {
+            "H": 1,
+            "W": 2,
+            "WH": 3,
+            "WWH": 5,
+            "WWWH": 7,
+            "WWWWH": 9,
+        }
+
+        for interval in scale_interval:
+            num_steps = node_interval.get(interval)
+
+            for i in range(0, num_steps):
                 node = node.next
-                nodes.append(node)
-            else:
-                print("Error")
+            nodes.append(node)
 
         nodes = map(str, nodes)
-        return " -> ".join(nodes)
+        print(" -> ".join(nodes))
 
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.previous = None
 
     def __repr__(self):
         return self.data
--- CircularLinkedList_Script.py
+++ CircularLinkedList_Script.py
@@ -1,25 +1,22 @@
 from CircularLinkedList import CircularLinkedList as CLL
-import copy
 
-C_chromatic = CLL(["C",
-                   "C#/Db",
-                   "D",
-                   "D#/Eb",
-                   "E",
-                   "F",
-                   "F#/Gb",
-                   "G",
-                   "G#/Ab",
-                   "A",
-                   "A#/Bb",
-                   "B"])
-print(C_chromatic)
-C_major = C_chromatic.major_scale()
-print(C_major)
+chromatic_scale = CLL(("C",
+                       "C#/Db",
+                       "D",
+                       "D#/Eb",
+                       "E",
+                       "F",
+                       "F#/Gb",
+                       "G",
+                       "G#/Ab",
+                       "A",
+                       "A#/Bb",
+                       "B"))
+print(chromatic_scale)
 
-D_chromatic = copy.deepcopy(C_chromatic)
-D_chromatic.head = D_chromatic.head.next.next
-
-print(D_chromatic)
-D_major = D_chromatic.major_scale()
-print(D_major)
+chromatic_scale.generate_scale("C", "major")
+chromatic_scale.generate_scale("A", "minor")
+chromatic_scale.generate_scale("C", "pentatonic")
+chromatic_scale.generate_scale("C", "fifths")
+chromatic_scale.generate_scale("C", "fourths")
+chromatic_scale.generate_scale("C", "relative minor")
