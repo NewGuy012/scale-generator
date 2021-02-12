318c572d6cbf2ba8db0cbc6cf26684729fc5c395
NewGuy012 <juyoung.m.yoo@gmail.com>
2021-02-12 14:50:41 -0800

    Rename main script.

---
+++ ScaleGenerator.py
@@ -0,0 +1,37 @@
+from CircularLinkedList import CircularLinkedList as CLL
+
+chromatic_scale = CLL(("C",
+                       "C#/Db",
+                       "D",
+                       "D#/Eb",
+                       "E",
+                       "F",
+                       "F#/Gb",
+                       "G",
+                       "G#/Ab",
+                       "A",
+                       "A#/Bb",
+                       "B"))
+print("12 Tone, Equal Temperament")
+print(chromatic_scale)
+print("\n")
+
+print("Scales")
+chromatic_scale.generate_scale("C", "major")
+chromatic_scale.generate_scale("C", "minor")
+chromatic_scale.generate_scale("C", "relative minor")
+chromatic_scale.generate_scale("A", "minor")
+chromatic_scale.generate_scale("C", "pentatonic")
+chromatic_scale.generate_scale("C", "fifths")
+chromatic_scale.generate_scale("C", "fourths")
+print("\n")
+
+print("Chords")
+chromatic_scale.generate_chord("C", "major", "triad")
+chromatic_scale.generate_chord("C", "major", "7")
+chromatic_scale.generate_chord("C", "dominant", "7")
+chromatic_scale.generate_chord("C", "minor", "triad")
+chromatic_scale.generate_chord("C", "diminished", "triad")
+chromatic_scale.generate_chord("C", "augmented", "triad")
+chromatic_scale.generate_chord("C", "dominant", "7")
+print("\n")
