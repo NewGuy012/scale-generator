e44e24a7fc6cda41c86e68ee87ace13d7799b288
jojojoleeleelee <vernapii@gmail.com>
2021-02-13 02:14:22 -0800

    build basic layout

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
-
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+# Builder.load_file('generator.kv')
 # basic plan is to have a scale generator that looks something like this --
 
 # scale generator title banner
 # input Key (TextInput) and also a scale type select dropdown
 # submit button
 # extra text grid on the top after button is pressed
-class MyGridLayout(GridLayout):
-  def __init__(self, **kwargs):
-    # call grid layout in the constructor
-    super(MyGridLayout, self).__init__(**kwargs)
+class GeneratorGrid(Widget):
+    # def __init__(self):
+    #     self.key = ""
+    #     self.scale = ""
 
-    self.cols = 1
-    # input box
-    self.key = TextInput(multiline=False)
-    self.header = Label(text="Let's generate any scale!")
-    self.add_widget(self.header)
-    self.add_widget(self.key)
+    def key_typed(self, value):
+        self.key = value
 
-    # submit button and bind button
-    self.submit = Button(text="Generate Key!")
-    self.submit.bind(on_press=self.press)
-    self.add_widget(self.submit)
-  
-  def press(self,instance):
-    key = self.key.text
+    def scale_clicked(self, value):
+        self.scale = value
 
-class ScaleGenerator(App):
-  def build(self):
-    return MyGridLayout()
+class GeneratorApp(App):
+    def build(self):
+        return GeneratorGrid()
 
 if __name__ == '__main__':
-  ScaleGenerator().run()
+    GeneratorApp().run()
