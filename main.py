a939ba995426841d25114b31f3d2b7ef21829577
jojojoleeleelee <vernapii@gmail.com>
2021-02-13 21:56:58 -0800

    bug fixes

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class GeneratorGrid(Widget):
   
   def __init__(self,**kwargs):
     super(GeneratorGrid,self).__init__(**kwargs)
-    self.invalid_count = 0
     self.generated = ""
     self.key = ""
     self.scale = ""
@@ -26,39 +25,32 @@ class GeneratorGrid(Widget):
   def key_typed(self, value):
     valid_keys = set(["A", "B", "C", "D", "E", "F", "G", "A#", "B#", "C#", "D#", "E#", "F#", "G#", "Ab", "Bb", "Cb", "Db", "Eb", "Fb", "Gb"])
 
-    if value.upper() in valid_keys:
-      self.invalid_count = 0
+    if value.title() in valid_keys:
       self.key = value
       self.ids.user_input.text = value
     else:
-      self.invalid_count += 1
-      if self.invalid_count > 3:
-        self.ids.user_input.text = "WHAT IS WRONG WITH YOU!? \nDO YOU NO SPEAK ENGLISH??"
-      else:
-        self.ids.user_input.text = 'Please type a valid key to start scale!'
+      self.ids.user_input.text = 'Please type a valid key to start scale!'
     
-    self.ids.hint_text.text = f"Invalid hits: {self.invalid_count}" 
 
   def scale_clicked(self, value):
     error_message = set(["We cannot proceed until you type a valid key -_-;;", 'Please type a valid key to start scale!', "WHAT IS WRONG WITH YOU!? \nDO YOU NO SPEAK ENGLISH??"])
     scales = set(["Major", "Minor", "Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian", "Pentatonic", "Fourths", "Fifths", "Relative Minor"])
     prior = self.ids.user_input.text
     res = ''
+    
     if prior in error_message:
       res = "We cannot proceed until you type a valid key -_-;;"
     elif ' ' in prior:
-      self.ids.hint_text.text = "" 
-      self.ids.hint_text.text = prior
       key, scale = prior.split()
       self.scale = value
       scale = value
       res = key + " " + scale
     elif prior in scales:
-      self.ids.hint_text.text = "" 
+      self.ids.user_input.text = "" 
       self.scale = value
       res = value
     else:
-      self.ids.hint_text.text = "" 
+      self.ids.user_input.text = "" 
       self.scale = value
       res = prior + " " + value
     
@@ -70,11 +62,8 @@ class GeneratorGrid(Widget):
       self.ids.header.text = "INVALID ENTRY! TRY AGAIN BOOHOO"
     # this must equal the output of ScaleGenerator.py
     else:
-      self.generated = chromatic_scale.generate_scale(self.key.upper(), self.scale.lower())
+      self.generated = chromatic_scale.generate_scale(self.key.title(), self.scale.lower())
       self.ids.header.text = self.generated
-      # clear values
-      self.key = ""
-      self.scale = ""
 
 class GeneratorApp(App):
   def build(self):
