e84808668df9f6fc5aac8cddc127626b9def5f7a
NewGuy012 <juyoung.m.yoo@gmail.com>
2021-04-13 03:16:45 -0700

    Add key signature function.

--- CircularDoublyLinkedList.py
+++ CircularDoublyLinkedList.py
@@ -1,14 +1,11 @@
-from itertools import tee
-
-
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
+from more_itertools import pairwise
 
 
 class Node:
+    """
+    A class used to represent a Node in a Doubly Linked List.
+    """
+
     def __init__(self, data):
         self.data = data
         self.prev = None
@@ -19,10 +16,21 @@ class Node:
 
 
 class CircularDoublyLinkedList:
+    """A class used to represent a Circular Doubly Linked List"""
+
     def __init__(self, data=None, starting_data=None):
+        """
+        Parameters
+        ----------
+        data : list of str
+            A list of data to initialize the linked list
+        starting_data : str
+            The starting node data
+        """
         nodes = [Node(item) for item in data]
 
-        starting_index = data.index(starting_data)
+        starting_index = [idx for idx, elem in enumerate(data)
+                          if starting_data == elem][0]
         self.head = nodes[starting_index]
 
         for current_node, next_node in pairwise(nodes):
@@ -46,6 +54,8 @@ class CircularDoublyLinkedList:
 class MusicalScale:
     chromatic_notes = ["C", "(C#/Db)", "D", "(D#/Eb)", "E", "F",
                        "(F#/Gb)", "G", "(G#/Ab)", "A", "(A#/Bb)", "B"]
+
+    sharp_keys = ["C", "G", "D", "A", "E", "B", "(F#/Gb)"]
 
     step_intervals = {
         "H": 1,
@@ -96,22 +106,25 @@ class MusicalScale:
 
     @ classmethod
     def chromatic_scale(cls, starting_note):
+        """Class method docstrings go here."""
         return cls(cls.chromatic_notes, starting_note)
 
-    def letter2num(cls, interval_list):
+    def letter2num(self, interval_list):
+        """Class method docstrings go here."""
         num_list = []
 
         for i, interval in enumerate(interval_list):
             num_semitones = 0
 
             for letter in interval:
-                num_semitones += cls.step_intervals.get(letter)
+                num_semitones += self.step_intervals.get(letter)
 
             num_list.append(num_semitones)
 
         return num_list
 
     def generate_scale(self, scale_interval):
+        """Class method docstrings go here."""
         interval_list = self.scale_intervals.get(scale_interval)
         interval_list = self.letter2num(interval_list)
         interval_list.pop()
@@ -135,6 +148,7 @@ class MusicalScale:
             return new_nodes
 
     def generate_chord(self, scale_interval, chord_interval):
+        """Class method docstrings go here."""
         scale = self.generate_scale(scale_interval)
 
         interval_list = self.chord_intervals.get(chord_interval)
@@ -150,7 +164,7 @@ class MusicalScale:
 
             new_notes.append(node.data)
 
-        new_nodes = CircularDoublyLinkedList(new_notes, scale.head.data)
+        new_nodes = CircularDoublyLinkedList(new_notes, self.starting_note)
 
         if chord_interval == "diatonic":
             if scale_interval == "major":
@@ -171,3 +185,66 @@ class MusicalScale:
                     node = node.next
 
         return new_nodes
+
+    def generate_signature(self, key):
+        C_chromatic_scale = MusicalScale.chromatic_scale("C")
+        scale_fifths = C_chromatic_scale.generate_scale("fifths")
+
+        current_node = scale_fifths.head
+        num_sharps = 0
+        num_flats = 0
+
+        while key != current_node.data:
+            if key in MusicalScale.sharp_keys:
+                num_sharps += 1
+                current_node = current_node.next
+            else:
+                num_flats += 1
+                current_node = current_node.prev
+
+        return num_sharps, num_flats
+
+
+def generate_musical_scale(keys=["C"], scales=["major"], chords=["triad"]):
+    """This is the summary line
+
+    This is the further elaboration of the docstring. Within this section,
+    you can elaborate further on details as appropriate for the situation.
+    Notice that the summary and the elaboration is separated by a blank new
+    line.
+    """
+
+    for key in keys:
+        key_chromatic_scale = MusicalScale.chromatic_scale(key)
+        # output_str = f"{key} chromatic: {key_chromatic_scale}"
+        # print(output_str)
+
+        num_sharps, num_flats = key_chromatic_scale.generate_signature(key)
+        output_str = f"{key} signature: {num_sharps}#/{num_flats}b"
+        print(output_str)
+
+        for scale in scales:
+            key_scale = key_chromatic_scale.generate_scale(scale)
+
+            if scale in ["pentatonic", "fourths", "fifths", "relative minor"]:
+                output_str = f"{key} {scale} scale: {key_scale}"
+                print(output_str)
+            else:
+                output_str = f"{key} {scale} {'scale':>5}: {key_scale}"
+                print(output_str)
+
+            if scale in ["major", "minor"]:
+                for chord in chords:
+                    key_chord = key_chromatic_scale.generate_chord(
+                        scale, chord)
+                    output_str = f"{key} {scale} {chord:>5}: {key_chord}"
+                    print(output_str)
+
+            if scale in ["dominant", "diminished", "augmented"]:
+                for chord in chords:
+                    if chord != "diatonic":
+                        key_chord = key_chromatic_scale.generate_chord(
+                            scale, chord)
+                        output_str = f"{key} {scale} {chord:>5}: {key_chord}"
+                        print(output_str)
+            print("\n")
--- main.py
+++ main.py
@@ -1,52 +1,19 @@
-from CircularDoublyLinkedList import MusicalScale
+from CircularDoublyLinkedList import MusicalScale, generate_musical_scale
 
 
-def generate_musical_scale(keys=["C"], scales=["major"], chords=["triad"]):
-    # scales = ["major", "minor", "dominant", "diminished", "augmented",
-    #           "pentatonic", "fourths", "fifths", "relative minor"]
-    # chords = ["triad", "sus2", "sus4", "7th",
-    #           "9th", "11th", "13th", "diatonic"]
-
-    for key in keys:
-
-        key_chromatic_scale = MusicalScale.chromatic_scale(key)
-        output_str = f"{key} chromatic: {key_chromatic_scale}"
-        print(output_str)
-
-        for scale in scales:
-            key_scale = key_chromatic_scale.generate_scale(scale)
-
-            if scale in ["pentatonic", "fourths", "fifths", "relative minor"]:
-                output_str = f"{key} {scale} scale: {key_scale}"
-                print(output_str)
-            else:
-                output_str = f"{key} {scale} {'scale':>5}: {key_scale}"
-                print(output_str)
-
-            if scale in ["major", "minor"]:
-                for chord in chords:
-                    key_chord = key_chromatic_scale.generate_chord(
-                        scale, chord)
-                    output_str = f"{key} {scale} {chord:>5}: {key_chord}"
-                    print(output_str)
-
-            if scale in ["dominant", "diminished", "augmented"]:
-                for chord in chords:
-                    if chord != "diatonic":
-                        key_chord = key_chromatic_scale.generate_chord(
-                            scale, chord)
-                        output_str = f"{key} {scale} {chord:>5}: {key_chord}"
-                        print(output_str)
-            print("\n")
-        print("\n")
-
-
-if __name__ == "__main__":
-    keys_list = ["C", "D"]
-    scales_list = ["major", "minor", "dominant", "diminished", "augmented",
-                   "pentatonic", "fourths", "fifths", "relative minor"]
-    chords_list = ["triad", "sus2", "sus4", "7th",
-                   "9th", "11th", "13th", "diatonic"]
+def main():
+    # keys_list = ["C", "D"]
+    # scales_list = ["major", "minor", "dominant", "diminished", "augmented",
+    #                "pentatonic", "fourths", "fifths", "relative minor"]
+    # chords_list = ["triad", "sus2", "sus4", "7th",
+    #                "9th", "11th", "13th", "diatonic"]
+    keys_list = ["C", "D", "A", "F", "(A#/Bb)"]
+    scales_list = ["major"]
+    chords_list = ["triad"]
 
     generate_musical_scale(
         keys=keys_list, scales=scales_list, chords=chords_list)
+
+
+if __name__ == "__main__":
+    main()
